544cfb0fff627d01473e5a976684a3d46e24c5f6
Krystyna Shunkina <christina.shunkina@gmail.com>
2024-03-20 18:36:49 +0200

    hw32-depl

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './styles.css'
-import React from 'react'
-import ReactDOM from 'react-dom/client';
-import App from './App.js';
-const mainEl = document.getElementById('root');
-const root = ReactDOM.createRoot(mainEl)
-root.render(<App />)
